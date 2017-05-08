35efd336099a3c2f05e80091ad454055e05e7ae9
Aditya Kurhade <adikrhd@gmail.com>
2017-05-08 22:24:43 +0530

    simple linear regression is fit to training set

--- simple_linear_regression.R
+++ simple_linear_regression.R
@@ -14,3 +14,10 @@ test_set = subset(dataset, split == FALSE)
 # Feature Scaling    #we are not going to use feature scaling in simple linear regression bcz the library we are going to use will take care of that
 # training_set = scale(training_set)
 # test_set = scale(test_set)
+
+# Fitting Simple linear regression to Training set
+
+regressor = lm(formula = Salary ~ YearsExperience,
+               data = training_set) #after running this command we can see the summary of the regressor by using 'summary(regressor)' which includes some statistical information
+
+
