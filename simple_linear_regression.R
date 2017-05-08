3e9321430d6a55ab6dcc2991a5069c1957e1c9e8
root <root@localhost.localdomain>
2017-05-08 22:09:50 +0530

    data is preprocessed

--- simple_linear_regression.R
+++ simple_linear_regression.R
@@ -1,16 +1,16 @@
 # Data Preprocessing Template
 
 # Importing the dataset
-dataset = read.csv('Data.csv')
+dataset = read.csv('Salary_Data.csv')
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
-library(caTools)
+#library(caTools)
 set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
-# Feature Scaling
+# Feature Scaling    #we are not going to use feature scaling in simple linear regression bcz the library we are going to use will take care of that
 # training_set = scale(training_set)
 # test_set = scale(test_set)
