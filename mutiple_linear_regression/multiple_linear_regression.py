f4c56e216487834b785fcac9c8303884c6217fb8
Aditya Kurhade <adikrhd@gmail.com>
2017-05-10 19:50:32 +0530

    categorical data encoded in multiple linear regression

--- mutiple_linear_regression/multiple_linear_regression.py
+++ mutiple_linear_regression/multiple_linear_regression.py
@@ -10,6 +10,19 @@ dataset = pd.read_csv('50_Startups_MultipleLinearRegression.csv')
 X = dataset.iloc[:, :-1].values # Indepentent Variables R&D spend,Administration,Marketing spend,state
 y = dataset.iloc[:, 4].values # dependent variables Profit
 
+                
+# Encoding categorical variables from data set we can see that there is only one column which is of independent variable 'states' has to be encoded
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder 
+                
+labelencoder_X = LabelEncoder()
+X[:,3] = labelencoder_X.fit_transform(X[:,3])
+
+#In this case it is good to encode categorical variables to specific mathematical no. but it should not happen that machine would compare this categories according to the ascending or descending order of the no,. is given
+# to avoid this we will use dummy variables which will include column for each category and if the value is equal to that category we will make that specific row '1'
+onehotencoder = OneHotEncoder(categorical_features= [3]) #onehotencoder actually creates object to convert categorical variable into columns
+X = onehotencoder.fit_transform(X).toarray(); # simply fitting data into object
+                
+                
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
