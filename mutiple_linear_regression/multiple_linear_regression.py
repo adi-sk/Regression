849a7b6ec9e6af2cec2505426cd798c689380150
Aditya Kurhade <adikrhd@gmail.com>
2017-05-11 21:19:18 +0530

    data is fit into multiple linear regression

--- mutiple_linear_regression/multiple_linear_regression.py
+++ mutiple_linear_regression/multiple_linear_regression.py
@@ -22,15 +22,27 @@ X[:,3] = labelencoder_X.fit_transform(X[:,3])
 onehotencoder = OneHotEncoder(categorical_features= [3]) #onehotencoder actually creates object to convert categorical variable into columns
 X = onehotencoder.fit_transform(X).toarray(); # simply fitting data into object
                 
+# Avoiding the dummy variable trap
+# Mostly python provides some libraries which take care of dummy varible trap
+
+X = X[:,1:] # We are just avoiding dummy variable trap here basically what we are doing is we are avoid one of the dummy variable to be in equation                               
                 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
+# Feature Scaling   # library is going to take care of feature scaling 
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+                            
+# Fitting Multiple linear regression to training set                            
+# we are going to import the same class what we did in simple linear regression but the difference here will be we will create linear regression model for every independent variable present here
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
