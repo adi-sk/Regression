014e0b240c7ed004ecc197707acc3799af6f458f
Aditya Kurhade <adikrhd@gmail.com>
2017-05-13 21:02:22 +0530

    Encoding and split done in R Multiple Linear Regression

--- mutiple_linear_regression/multiple_linear_regression.R
+++ mutiple_linear_regression/multiple_linear_regression.R
@@ -1,13 +1,19 @@
 # Data Preprocessing Template
 
 # Importing the dataset
-dataset = read.csv('Data.csv')
+dataset = read.csv('50_Startups_MultipleLinearRegression.csv')
+
+#Encoding Categorical Data
+dataset$State = factor(dataset$State,
+                       levels = c('New York','California','Florida'),
+                       labels = c(1,2,3))
+
 
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
-library(caTools)
+#library(caTools)
 set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
