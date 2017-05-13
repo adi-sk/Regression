7ad1eb04cacbd352d932c76521cdcfe939a26d4a
Aditya Kurhade <adikrhd@gmail.com>
2017-05-13 21:30:00 +0530

    Fitting Multiple linear Regression Done on Training set

--- mutiple_linear_regression/multiple_linear_regression.R
+++ mutiple_linear_regression/multiple_linear_regression.R
@@ -17,6 +17,16 @@ split = sample.split(dataset$Profit, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
-# Feature Scaling
+# Feature Scaling  # feature scaling is not required here library will take care of that
 # training_set = scale(training_set)
 # test_set = scale(test_set)
+
+#Fitting Multiple Linear Regression to Training set
+
+#regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State)# R replaces ' ' with '.'
+                          # OR we can use
+regressor = lm(formula = Profit ~ ., # '.' includes all the independent variables
+               data = training_set)  # the best part of the R regressor is it doesn't fall in dummy variable trap it automatically removes one dummy variable while forming model
+summary(regressor) # if we see the summary we can find that only siginificant variable here is 'R.D.Spend' it is the only variable which has p value < 0.05
+
+# that means we can replace our regression formula with Profit ~ R.D.Spend
