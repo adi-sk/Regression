eeec7efa4505f3bd14e11e32b1cd23445034f31e
Aditya Kurhade <adikrhd@gmail.com>
2017-06-15 18:25:06 -0400

    decision tree model adde for R

--- decision_tree_regression/decision_tree_regression.R
+++ decision_tree_regression/decision_tree_regression.R
@@ -1,4 +1,4 @@
-# Regression Template
+# Decision Tree Regression 
 
 # Importing the dataset
 dataset = read.csv('Position_Salaries.csv')
@@ -16,33 +16,38 @@ dataset = dataset[2:3]
 # training_set = scale(training_set)
 # test_set = scale(test_set)
 
-# Fitting the Regression Model to the dataset
+# Fitting the Decisioin Tree Regression Model to the dataset
 # Create your regressor here
+# install.packages('rpart')
+# library(rpart)
+regressor = rpart(formula = Salary ~ . ,
+                  data = dataset,
+                  control = rpart.control(minsplit = 2))
 
 # Predicting a new result
-y_pred = predict(regressor, data.frame(Level = 6.5))
+y_pred = predict(regressor, data.frame(Level = 6.5)) # this may not predict the right result So to conclude Decision Tree regression model is not the interesting model in 1D but it is more powerful model in more dimensions
 
-# Visualising the Regression Model results
-# install.packages('ggplot2')
-library(ggplot2)
+# Visualising the Decisioin Tree Regression Model results NOTE : this does not give the right graph see python explaination solution for this is to visualize the graph in high resolution 
+#install.packages('ggplot2')
+#library(ggplot2)
 ggplot() +
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
              colour = 'red') +
   geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)),
             colour = 'blue') +
-  ggtitle('Truth or Bluff (Regression Model)') +
+  ggtitle('Truth or Bluff (Decisioin Tree Regression Model)') +
   xlab('Level') +
   ylab('Salary')
 
-# Visualising the Regression Model results (for higher resolution and smoother curve)
+# Visualising the Decisioin Tree Regression Model results (for higher resolution and smoother curve)
 # install.packages('ggplot2')
-library(ggplot2)
-x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+#library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
 ggplot() +
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
              colour = 'red') +
   geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
             colour = 'blue') +
-  ggtitle('Truth or Bluff (Regression Model)') +
+  ggtitle('Truth or Bluff (Decisioin Tree Regression Model)') +
   xlab('Level') +
   ylab('Salary')
